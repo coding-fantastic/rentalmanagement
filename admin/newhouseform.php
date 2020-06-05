3867992e9afae81e022414f9e529b2d29eb83946
coding-fantastic <alexnzioka@protonmail.com>
2020-06-05 14:06:56 +0300

    uppercase first character

--- admin/newhouseform.php
+++ admin/newhouseform.php
@@ -10,7 +10,11 @@
     // If form submitted , insert values into the database.
     if (isset($_REQUEST['housenumber'])){
       $housenumber = trim($_REQUEST['housenumber']);
+      // uppercase first character of each word in a string
+      $housenumber = ucwords($housenumber);
       $features = trim($_REQUEST['features']);
+      // uppercase first character of each word in a string
+      $features = ucwords($features);
       $rent = trim($_REQUEST['rent']);
       $status = trim($_REQUEST['status']);
       // echo "housenumber:".$housenumber."-features".$features."-rent".$rent."-status".$status;
