dbd8cfa84c670776c136d731b545247d2a0cab0b
coding-fantastic <alexnzioka@protonmail.com>
2020-06-11 16:27:42 +0300

    now i can fetch from db and display

--- try/housesform.php
+++ try/housesform.php
@@ -6,6 +6,13 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script> -->
+  <!-- datatable cdn start  -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+           <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+           <script src="https://cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"></script>
+           <script src="https://cdn.datatables.net/1.10.12/js/dataTables.bootstrap.min.js"></script>
+           <link rel="stylesheet" href="https://cdn.datatables.net/1.10.12/css/dataTables.bootstrap.min.css" />
+  <!-- datatable cdn end -->
   <link rel="stylesheet" href="../try/bootstrap/css/bootstrap.min.css">
   <script src="../try/jquery/jquery.min.js"></script>
   <script src="../try/popper/popper.min.js"></script>
@@ -13,12 +20,42 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
 </head>
 <body class="jumbotron">
+  <!-- start of navigation bar  -->
+
+
+  <!-- end of navigation bar -->
+
+
+  <?php
+  require('../database/db.php');
+  // If form submitted , insert values into the database.
+  if (isset($_REQUEST['housenumber'])){
+    $housenumber = trim($_REQUEST['housenumber']);
+    // uppercase first character of each word in a string
+    $housenumber = ucwords($housenumber);
+    $features = trim($_REQUEST['features']);
+    // uppercase first character of each word in a string
+    $features = ucwords($features);
+    $rent = trim($_REQUEST['rent']);
+    $status = trim($_REQUEST['status']);
+    // echo "housenumber:".$housenumber."-features".$features."-rent".$rent."-status".$status;
+    $query = "insert into `houses` (house_number, features, status, rent) VALUES ('$housenumber','$features','$status','$rent')";
+    $result = mysqli_query($con, $query);
+
+    if($result){
+      echo "<div class='form'>
+      <h3>You have registered successfully.</h3>
+      ";
+    }
+    }else{
+    ?>
 
   <div class="container ">
+
     <div class="container">
       <h2>Houses</h2>
       <p>You can add a new house by clicking the new house button.</p>
-      <button  type="button" class="btn btn-primary" data-toggle="collapse" data-target="#demo"><span class="glyphicon glyphicon-plus"></span> New house</button>
+      <button  type="button" class="btn btn-primary" data-toggle="collapse" data-target="#demo"><span class="glyphicon glyphicon-plus"></span> New house </button>
       <div id="demo" class="collapse hide">
         <div class="col-md-12 ">
           <div class="card">
@@ -27,7 +64,7 @@
             </div>
             <div class="card-body">
               <div class="form-group">
-                <form  action="" method="post">
+                <!-- <form   action="" method="post">
                   <input class="form-control" type="text" name="housenumber" placeholder="housenumber" required> <br>
                   <textarea class="form-control" name="features" placeholder="features" rows="3" cols="80"></textarea> <br>
                   <input class="form-control" type="text" name="rent" placeholder="rent" > <br>
@@ -35,14 +72,29 @@
                     <option value="vacant">vacant</option>
                     <option value="occupied">occupied</option>
                   </select>  <br>
-                  <input class="btn btn-primary" type="submit" name="submit" value="Register">
+                  <input class="btn btn-primary" type="submit" name="submit" value="Register"> <br>
+
+
+                </form> -->
+                <form class="" action="" method="post">
+                  <input class="form-control" type="text" name="housenumber" placeholder="housenumber" required> <br>
+                  <textarea class="form-control" name="features" placeholder="features" rows="3" cols="80" ></textarea> <br>
+                  <input class="form-control" type="text" name="rent" placeholder="rent" >  <br>
+                  <select class="form-control" name="status" >
+                    <option value="vacant">vacant</option>
+                    <option value="occupied">occupied</option>
+                  </select>  <br>
+                  <input class="btn btn-primary" type="submit" name="submit" value="Register" >
+
                 </form>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div> <br>
+    </div>
+    <?php }?>
+    <br>
 
 
 
@@ -53,40 +105,47 @@
             <h2>View available houses</h2>
           </div>
           <div class="card-body">
-            <table class="table table-hover">
-              <thead>
-                <tr>
-                  <th>House number</th>
-                  <th>Features</th>
-                  <th>Rent</th>
-                  <th>Status</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>A1</td>
-                  <td>2 bedroom , kitchen and bathroom </td>
-                  <td>15000</td>
-                  <td>Occupied</td>
-                </tr>
-                <tr>
-                  <td>A2</td>
-                  <td>2 bedroom , kitchen garden , bathroom and toilet separate</td>
-                  <td>20000</td>
-                  <td>Empty</td>
-                </tr>
-              </tbody>
-            </table>
+              <!--  datatable  start-->
+            <div class="table-responsive">
+              <table id="housestable" class="table table-striped table-bordered">
+                    <thead>
+                      <tr>
+                        <td>ID</td>
+                        <td>House Number</td>
+                        <td>Features</td>
+                        <td>Rent</td>
+                        <td>Status</td>
+                      </tr>
+                    </thead>
+                    <?php
+                    $query= "select * from houses order by id desc";
+                    $result=mysqli_query($con, $query);
+                    while ($row = mysqli_fetch_array($result)){
+                      echo '<tr>
+                                <td>'.$row["id"].'</td>
+                                <td>'.$row["house_number"].'</td>
+                                <td>'.$row["features"].'</td>
+                                <td>'.$row["rent"].'</td>
+                                <td>'.$row["status"].'</td>
+                            </tr>';
+                    }
+                     ?>
+              </table>
+            </div>
+
+            <!-- datatable end -->
           </div>
         </div>
       </div>
     </div>
-
-
-
-
   </div>
+<script>
+$(document).ready(function () {
+  $('#housestable').DataTable();
 
+  // check https://stackoverflow.com/questions/23101966/bootstrap-alert-auto-close for success alert
+});
+</script>
 
 </body>
 </html>
