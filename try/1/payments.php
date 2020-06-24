5c72f57cf6ae16b06a94fe284741a249575e12a2
coding-fantastic <alexnzioka@protonmail.com>
2020-06-24 15:24:00 +0300

    playing with w3 grid trying to fit items in a container

--- try/1/payments.php
+++ try/1/payments.php
@@ -2,17 +2,62 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>Payments Form</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+      <title>Payments Form</title>
+
   </head>
-  <body>
-    start of navbar
-    <div class="w3-bar w3-black">
-      <a href="#" class="w3-bar-item w3-button">Home</a>
-      <a href="#" class="w3-bar-item w3-button">Link 1</a>
-      <a href="#" class="w3-bar-item w3-button">Link 2</a>
-      <a href="#" class="w3-bar-item w3-button">Link 3</a>
+  <body class="w3-light-grey">
+    <!-- start of navbar -->
+    <?php
+    include 'navbar.php';
+      ?>
+    <!-- end of navbar -->
+
+    <!-- modal div -->
+    <div class="w3-col m3 w3-green">
+      <!-- Trigger/Open the Modal -->
+      <button onclick="document.getElementById('id01').style.display='block'"
+      class="w3-button">Open Modal</button>
+
+      <!-- The Modal -->
+      <div id="id01" class="w3-modal">
+        <div class="w3-modal-content">
+          <div class="w3-container">
+            <span onclick="document.getElementById('id01').style.display='none'"
+            class="w3-button w3-display-topright">&times;</span>
+            <p>Some text in the Modal..</p>
+            <p>Some text in the Modal..</p>
+          </div>
+        </div>
+      </div>
+
+
     </div>
+
+    <!-- display current payments -->
+    <div class="w3-panel">
+
+
+    </div>
+
+    <div class="w3-container">
+      <div class="w3-row">
+        <div class="w3-col m10 l5 w3-green">
+          <p>This is an outer div</p>
+          <div class="w3-col m6 l4 w3-blue">
+            <p>this is an inner div</p>
+
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+
+    <script type="text/javascript">
+
+
+    </script>
+
   </body>
 </html>
