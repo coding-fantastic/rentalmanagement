73a9f14057f6b5d055c16052e871b8d9d7f62cff
coding-fantastic <alexnzioka@protonmail.com>
2020-06-26 00:34:51 +0300

    update

--- try/1/navbar.php
+++ try/1/navbar.php
@@ -6,9 +6,13 @@ require '../../database/db.php';
  <html lang="en" dir="ltr">
    <head>
      <meta charset="utf-8">
-       <title>Payments Form</title>
+       <title>RMS</title>
 
        <!-- datatables cdn -->
+       <!-- start icons -->
+       <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+      <!--end icons -->
        <link rel="stylesheet" href="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css">
        <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
        <script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
@@ -22,6 +26,7 @@ require '../../database/db.php';
 
  <meta name="viewport" content="width=device-width, initial-scale=1">
  <link rel="stylesheet" href="css/w3.css">
+
 <!-- bootstrap 4 -->
 <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
@@ -37,10 +42,10 @@ require '../../database/db.php';
 
 <!-- Top container -->
 <div class="w3-bar w3-top w3-black w3-large" style="z-index:4">
-  <a href="http://localhost/websites/rentalms/try/1/dashboard.php"><button class="w3-bar-item w3-button w3-hover-none w3-hover-text-light-grey" ><i class="fa fa-bars"></i>  Menu</button></a>
+  <a href="http://localhost/websites/rentalms/try/1/dashboard.php"><button class="w3-bar-item w3-button w3-hover-none w3-hover-text-light-grey" ><i class="fa fa-bars"></i>  Home</button></a>
 
   <span class="w3-bar-item w3-right">Logo</span>
-</div>
+</div><br>
 
 
 <!-- <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
