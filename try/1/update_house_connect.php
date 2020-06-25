76e6d887a83e32d264596f6020c8e73d9cd4e605
coding-fantastic <alexnzioka@protonmail.com>
2020-06-26 00:40:36 +0300

    connect update form with db

---
+++ try/1/update_house_connect.php
@@ -0,0 +1,29 @@
+<?php
+require('../../database/db.php');
+// If form submitted , insert values into the database.
+if (isset($_REQUEST['housenumber'])){
+  $housenumber = trim($_REQUEST['housenumber']);
+  // uppercase first character of each word in a string
+  $housenumber = ucwords($housenumber);
+  $features = ucwords(trim($_REQUEST['features']));
+  $id = trim($_REQUEST['id']);
+  $status = ucwords(trim($_REQUEST['status']));
+
+  echo $id."<br>". $housenumber."<br>".$features."<br>".$status;
+
+
+
+  $query = "UPDATE   `houses` SET house_number='$housenumber', features='$features', status='$status' WHERE id=$id";
+  $result = mysqli_query($con, $query);
+
+  if($result){
+
+    echo "<div class='form'>
+    <h3>You have paid successfully.</h3>  </div>";
+    sleep(2);
+    header ("Location: http://localhost/websites/rentalms/try/1/houses.php");
+  }
+}else {
+  echo "<br>housenumber was not set thats why you didn't get results as expected<br>";
+  echo "housenumber ->".$housenumber;
+}
