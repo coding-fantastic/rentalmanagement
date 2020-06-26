3d2b048dbbbe7129765a386b5d28198a221f361e
coding-fantastic <alexnzioka@protonmail.com>
2020-06-26 14:59:16 +0300

    login page

---
+++ try/1/login.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<title>W3.CSS</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<body>
+
+  <div class="w3-container ">
+    <form class="w3-container w3-card-4" action="loginConnect.php" method="post">
+      <h2 class="w3-text-blue">Login</h2>
+      <p>Use any of the w3-text-color classes to color your labels.</p>
+      <p>
+      <label class="w3-text-blue"><b>Username</b></label>
+      <input class="w3-input w3-border" name="username" type="text"></p>
+      <p>
+      <label class="w3-text-blue"><b>Password</b></label>
+      <input class="w3-input w3-border" name="password" type="password"></p>
+      <p>
+      <button class="w3-btn w3-blue">Login</button></p>
+    </form>
+
+  </div>
+
+
+
+</body>
