a49ec0797390b8138c787f9d39cf1deb6770a9b0
coding-fantastic <alexnzioka@protonmail.com>
2020-06-27 20:00:15 +0300

    can add new tenant but won't update status in house table

--- try/1/tenantformConnect.php
+++ try/1/tenantformConnect.php
@@ -1,5 +1,8 @@
 <?php
 require('../../database/db.php');
+
+include 'navbar.php';
+
 // If form submitted , insert values into the database.
 if (isset($_REQUEST['housenumber'])){
   $housenumber = trim($_REQUEST['housenumber']);
@@ -11,23 +14,98 @@ if (isset($_REQUEST['housenumber'])){
   $mobile_number = trim($_REQUEST['mobile_number']);
   $email = (trim($_REQUEST['email']));
   $registration_date = (trim($_REQUEST['registration_date']));
-  $status = (trim($_REQUEST['status']));
+  $status = ucwords((trim($_REQUEST['status'])));
 
 // echo $fullname." ".$gender." ".$national_id." ".$mobile_number." ".$email." ".$registration_date." ".$housenumber." ". $status;
-  $query = "INSERT  into `tenants`
-  (fullname, gender, national_id, mobile_number, email , registration_date , house_number, status)
-   VALUES
-  ('$fullname','$gender','$national_id','$mobile_number', '$email','$registration_date', ' $housenumber', '$status')";
-  $result = mysqli_query($con, $query);
 
-  if($result){
+  $houseExist=0;
 
-    echo "<div class='form'>
-    <h3>You have paid successfully.</h3>  </div>";
-    sleep(2);
-    header ("Location: http://localhost/websites/rentalms/try/1/tenants.php");
+  $query= "SELECT * from houses order by id desc ";
+  $result=mysqli_query($con, $query);
+  while ($row = mysqli_fetch_array($result)){
+    // check if that house exists
+    if ($housenumber == $row['house_number'] ) {
+      $houseExist = 1;
+      // new variable to hold house id to allow usage outside the while loop
+      $house_id = $row['id'];
+      // new variable to hold house status to allow usage outside the while loop
+      $house_status = $row['status'];
+      break;
   }
-}else {
-  echo "housenumber was not set";
-  echo "housenumber ->".$housenumber;
 }
+
+
+    // if now the house exists now continue if not print no house was found
+    if ($houseExist == 1) {
+      // check house status if it is vacant will insert to tenants table and update house table
+      if ($house_status == 'Vacant') {
+
+        $query = "INSERT  into `tenants`
+        (fullname, gender, national_id, mobile_number, email , registration_date , house_number, status)
+         VALUES
+        ('$fullname','$gender','$national_id','$mobile_number', '$email','$registration_date', ' $housenumber', '$status')";
+        $result = mysqli_query($con, $query) or die($query);
+
+        ?>
+       <div class="w3-panel w3-pale-green w3-border">
+         <p> House was vacant and it exists </p>
+         <?php echo "before update house number is " .$housenumber." and house status is ".$house_status; ?>
+       </div>
+       <?php
+       // update status in house table
+       $query = "UPDATE `houses` SET status='Occupied' WHERE id=$housenumber";
+       $result = mysqli_query($con, $query) or die($query);
+
+             
+
+
+        if($result){
+
+          ?>
+         <div class="w3-panel w3-pale-green w3-border">
+           <p> Record inserted  and updated successfully.</p>
+         </div>
+         <?php
+
+          header ("Location: http://localhost/websites/rentalms/try/1/tenants.php");
+        }
+      }else {
+
+          // if the house is occupied enter value to tenants table but don't update house status
+         ?>
+        <div class="w3-panel w3-pale-green w3-border">
+          <p> House was found but it is Occupied. Tenant registered successfully but house table not modified. </p>
+        </div>
+        <?php
+        $query = "INSERT  into `tenants`
+        (fullname, gender, national_id, mobile_number, email , registration_date , house_number, status)
+         VALUES
+        ('$fullname','$gender','$national_id','$mobile_number', '$email','$registration_date', ' $housenumber', '$status')";
+        $result = mysqli_query($con, $query) or die($query);
+
+        if($result){
+
+
+          header ("Location: http://localhost/websites/rentalms/try/1/tenants.php");
+      }
+
+    }
+      }else {
+        ?>
+          <div class="w3-panel w3-pale-green w3-border">
+            <p> There is no <?php echo $housenumber; ?> house .</p>
+          </div>
+          <?php
+      }
+
+    }
+else {
+  ?>
+    <div class="w3-panel w3-pale-green w3-border">
+      <p> Housenumber not set .</p>
+    </div>
+    <?php
+}
+
+
+?>
