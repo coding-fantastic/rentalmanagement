4032de5114d301d6b674923fd61968aafea1c311
coding-fantastic <alexnzioka@protonmail.com>
2020-07-02 11:26:19 +0300

    update

--- try/1/navbar.php
+++ try/1/navbar.php
@@ -8,6 +8,8 @@ require '../../database/db.php';
      <meta charset="utf-8">
        <title>RMS</title>
 
+
+
        <!-- datatables cdn -->
        <!-- start icons -->
        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
@@ -27,18 +29,6 @@ require '../../database/db.php';
  <meta name="viewport" content="width=device-width, initial-scale=1">
  <link rel="stylesheet" href="css/w3.css">
 
-<!-- bootstrap 4 -->
-<!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js" integrity="sha384-1CmrxMRARb6aLqgBO7yyAxTOQE2AKb9GfXnEo760AUcUmFx3ibVJJAzGytlQcNXd" crossorigin="anonymous"></script> -->
-
-<!-- <div class="w3-bar w3-black">
-  <a href="#" class="w3-bar-item w3-button">Home</a>
-  <a href="#" class="w3-bar-item w3-button">Link 1</a>
-  <a href="#" class="w3-bar-item w3-button">Link 2</a>
-  <a href="#" class="w3-bar-item w3-button">Link 3</a>
-</div> -->
-
 
 <!-- Top container -->
 <div class="w3-bar w3-top w3-black w3-large" style="z-index:4">
@@ -46,62 +36,3 @@ require '../../database/db.php';
 
   <span class="w3-bar-item w3-right">Logo</span>
 </div><br>
-
-
-<!-- <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
-      </li>
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
-  </div>
-</nav> -->
-
-
-<!-- *************************************************************************** -->
-
-<!-- test div placement with this  -->
-<!-- <div class="w3-container w3-blue">
-  <div class="w3-row"> -->
-    <!-- order of screen size in w3 small screen , medium and large  -->
-    <!-- <div class="w3-col m10 l12 w3-green">
-      <p>This is an outer div</p>
-      <div class="w3-col m6 l4 w3-blue">
-        <p>this is an inner div</p>
-
-      </div>
-
-    </div>
-
-  </div>
-
-</div><br> -->
-
-<!-- *************************************************************************** -->
