a586eab91bb0ec25a6e2adf9c848ce94800e32a1
coding-fantastic <alexnzioka@protonmail.com>
2020-07-06 00:53:27 +0300

    will display everything on a new paragraph  when button is clicked

--- try/1/ajax/callback.js
+++ try/1/ajax/callback.js
@@ -1,11 +1,32 @@
-var ourRequest = new XMLHttpRequest();
-ourRequest.open('GET', 'http://localhost/websites/restApiPhp/1/api/post/read.php');
-ourRequest.onload  = function () {
-  //parse to json 
-  var ourData = JSON.parse(ourRequest.responseText);
-  //load first object on the console
-  console.log(ourData[0]);
+var postContainer = document.getElementById('post-info')
+var btn = document.getElementById('btn');
 
-};
+btn.addEventListener("click", function () {
 
-ourRequest.send();
+  // start of ajax call
+  var ourRequest = new XMLHttpRequest();
+  ourRequest.open('GET', 'http://localhost/websites/restApiPhp/1/api/post/read.php');
+  ourRequest.onload  = function () {
+    //parse to json
+    var ourData = JSON.parse(ourRequest.responseText);
+    renderHTML(ourData);
+
+  };
+
+  ourRequest.send();
+  // End of ajax call
+
+
+});
+
+// display to the page
+function renderHTML(data) {
+  var htmlString = "";
+  // loop through  json data passed through the function and concatenate to htmlString variable
+  for (var i = 0; i < data.length; i++) {
+    htmlString+= "<p>" + data[i].title + " <- title post , body -> " + data[i].body + " author -> " + data[i].author + ".</p>";
+  }
+
+  postContainer.insertAdjacentHTML('beforeend', htmlString);
+
+}
