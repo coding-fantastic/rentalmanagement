9136664ea0c57259316ef5ec2a3330aac0b06fc8
coding-fantastic <alexnzioka@protonmail.com>
2020-07-05 23:58:58 +0300

    update

--- try/1/ajax/callback.js
+++ try/1/ajax/callback.js
@@ -1,7 +1,9 @@
 var ourRequest = new XMLHttpRequest();
 ourRequest.open('GET', 'http://localhost/websites/restApiPhp/1/api/post/read.php');
 ourRequest.onload  = function () {
-  var ourData = JSON.parse(ourRequest.responseText); 
+  //parse to json 
+  var ourData = JSON.parse(ourRequest.responseText);
+  //load first object on the console
   console.log(ourData[0]);
 
 };
