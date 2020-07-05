c7fcc3223fbfa668f8372d18763bff212dc5ad14
coding-fantastic <alexnzioka@protonmail.com>
2020-07-06 01:29:43 +0300

    hide button when clicked 3 times using javascript

--- try/1/ajax/callback.js
+++ try/1/ajax/callback.js
@@ -1,3 +1,4 @@
+var pageCounter = 1;
 var postContainer = document.getElementById('animal-info')
 var btn = document.getElementById('btn');
 
@@ -5,17 +6,24 @@ btn.addEventListener("click", function () {
 
   // start of ajax call
   var ourRequest = new XMLHttpRequest();
-  ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json');
+  ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-'+ pageCounter +'.json');
   ourRequest.onload  = function () {
     //parse to json
     var ourData = JSON.parse(ourRequest.responseText);
     renderHTML(ourData);
 
   };
+  // console.log(pageCounter);
 
   ourRequest.send();
+  pageCounter++;
   // End of ajax call
 
+  // if you press button three times the hide button will disaper
+  if(pageCounter > 3){
+    // btn.classList.add("hide-me");
+    document.getElementById('btn').style.visibility = "hidden";
+  }
 
 });
 
